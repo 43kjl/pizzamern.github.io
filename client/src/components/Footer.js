90cd30329fe5f6d0d55f24ad9614fc951ec8ddda
43kjl <88443683+43kjl@users.noreply.github.com>
2024-06-12 12:57:09 +0545

    first commit

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='container-fluid d-flex pt-3 justify-content-center align-items-center bg-dark' style={{height:"50px"}}>
+    <p className='text-light '>made with love by santa developer</p>
+    </div>
+  )
+}
+
+export default Footer
